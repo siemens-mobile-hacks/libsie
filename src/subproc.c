dc339d0a07f74fd53c480e33c059fc492a0ea1e4
Philip Minakov <sir.cadogan@protonmail.com>
2024-02-01 17:57:33 +0400

    Rewrite SUBPROC implementation

--- src/subproc.c
+++ src/subproc.c
@@ -1,24 +1,35 @@
 #include <swilib.h>
 
-#define SUBPROC_NAME "Sie_SubProc"
-#define SUBPROC_MAX_SLOTS 2
+#define SUBPROC_NAME        "Sie_SubProc"
+#define SUBPROC_PG_ID       0x50
+#define SUBPROC_MAX_MEMBERS 0x0A
+#define SUBPROC_START_CEPID (SUBPROC_PG_ID * 0x100)
 
-int SLOTS[SUBPROC_MAX_SLOTS];
-int CEPIDS[SUBPROC_MAX_SLOTS] = {0x4409, 0x4311};
-int PRIOS[SUBPROC_MAX_SLOTS] = {0x80, 0x80};
+unsigned int SLOTS[SUBPROC_MAX_MEMBERS];
 
 static void OnMsg(void);
 
 void Sie_SubProc_Init() {
-    for (unsigned int i = 0; i < SUBPROC_MAX_SLOTS; i++) {
-        SLOTS[i] = 0;
-        CreateGBSproc(CEPIDS[i], SUBPROC_NAME, OnMsg, PRIOS[i], 0);
+    PGROUP pg = {
+            SUBPROC_PG_ID,
+            "T_SIE",
+            0x80,
+            0x8000,
+            SUBPROC_MAX_MEMBERS + 1,
+    };
+    if (!GetGBSProcAddress(SUBPROC_START_CEPID)) {
+        CreateICL(&pg);
+        CreateGBSproc(SUBPROC_START_CEPID, SUBPROC_NAME, OnMsg, 0x100, 0);
+    }
+    zeromem(SLOTS, sizeof(unsigned int) * 10);
+    for (unsigned int i = 1; i <= SUBPROC_MAX_MEMBERS; i++) {
+        CreateGBSproc(SUBPROC_START_CEPID + i, SUBPROC_NAME, OnMsg, 0x80, 0);
     }
 }
 
 void Sie_SubProc_Destroy() {
-    for (unsigned int i = 0; i < SUBPROC_MAX_SLOTS; i++) {
-        KillGBSproc(CEPIDS[i]);
+    for (unsigned int i = 1; i <= SUBPROC_MAX_MEMBERS; i++) {
+        KillGBSproc(SUBPROC_START_CEPID + i);
     }
 }
 
@@ -39,14 +50,11 @@ static void OnMsg()
 
 unsigned int Sie_SubProc_Run(void *proc, void *data)
 {
-    int slot = 0;
-    while (slot < SUBPROC_MAX_SLOTS) {
-        if (!SLOTS[slot]) {
-            SLOTS[slot] = 1;
-            GBS_SendMessage(CEPIDS[slot], slot, 0, proc, data);
+    for (unsigned int i = 0; i < SUBPROC_MAX_MEMBERS; i++) {
+        if (!SLOTS[i]) {
+            SLOTS[i] = 1;
+            GBS_SendMessage(SUBPROC_START_CEPID + 1 + i, i, 0, proc, data);
             return 1;
-        } else {
-            slot++;
         }
     }
     return 0;
